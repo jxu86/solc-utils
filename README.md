eb2a160a6e9d592d81aafdb709b6a6c839b5b0da
JC86 <jxu1@kalengo.com>
2019-10-16 13:45:15 +0800

    add project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # solc-utils
 solidity utils
+
+```
+npm install solc-utils
+```
---
+++ data/dataUtil.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.0;
+
+library DataConvert {
+
+    function uint2str(uint _i) internal returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+}
